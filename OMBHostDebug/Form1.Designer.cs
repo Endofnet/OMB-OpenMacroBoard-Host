1864aea514c7c7a7b0b230dac50adacd070b3e6b
matthias.naumann <matthias.naumann@synergetik.de>
2020-12-15 11:05:57 +0100

    Minor UI changes

--- OMBHostDebug/Form1.Designer.cs
+++ OMBHostDebug/Form1.Designer.cs
@@ -40,12 +40,24 @@
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Location = new System.Drawing.Point(6, 22);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 23);
             this.comboBox1.TabIndex = 0;
@@ -53,7 +65,7 @@
             // 
             // bt_connect
             // 
-            this.bt_connect.Location = new System.Drawing.Point(139, 11);
+            this.bt_connect.Location = new System.Drawing.Point(133, 21);
             this.bt_connect.Name = "bt_connect";
             this.bt_connect.Size = new System.Drawing.Size(75, 23);
             this.bt_connect.TabIndex = 1;
@@ -65,9 +77,9 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(13, 41);
+            this.listBox1.Location = new System.Drawing.Point(6, 22);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(100, 109);
+            this.listBox1.Size = new System.Drawing.Size(100, 79);
             this.listBox1.TabIndex = 2;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
@@ -75,9 +87,9 @@
             // 
             this.listBox2.FormattingEnabled = true;
             this.listBox2.ItemHeight = 15;
-            this.listBox2.Location = new System.Drawing.Point(119, 40);
+            this.listBox2.Location = new System.Drawing.Point(62, 22);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(206, 109);
+            this.listBox2.Size = new System.Drawing.Size(188, 79);
             this.listBox2.TabIndex = 2;
             // 
             // ti_ui
@@ -87,7 +99,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(225, 186);
+            this.button1.Location = new System.Drawing.Point(12, 280);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 3;
@@ -97,7 +109,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(13, 186);
+            this.button2.Location = new System.Drawing.Point(183, 280);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 23);
             this.button2.TabIndex = 4;
@@ -107,7 +119,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(119, 186);
+            this.button3.Location = new System.Drawing.Point(289, 280);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(100, 23);
             this.button3.TabIndex = 4;
@@ -117,39 +129,126 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(119, 157);
+            this.textBox1.Location = new System.Drawing.Point(6, 22);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(206, 23);
+            this.textBox1.Size = new System.Drawing.Size(365, 23);
             this.textBox1.TabIndex = 5;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(13, 157);
+            this.button4.Location = new System.Drawing.Point(276, 51);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.Size = new System.Drawing.Size(95, 23);
             this.button4.TabIndex = 6;
             this.button4.Text = "Set Command";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 51);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(82, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Add Btn Dn";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(94, 51);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(82, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Add Btn Up";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Location = new System.Drawing.Point(12, 186);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(377, 88);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Button Command";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(182, 51);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(82, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Add Key";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 70);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(115, 110);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Buttons";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Controls.Add(this.bt_connect);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(215, 52);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Port";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.listBox3);
+            this.groupBox4.Controls.Add(this.listBox2);
+            this.groupBox4.Location = new System.Drawing.Point(133, 70);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(256, 110);
+            this.groupBox4.TabIndex = 12;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Button Settings";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 15;
+            this.listBox3.Location = new System.Drawing.Point(6, 22);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(50, 79);
+            this.listBox3.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 220);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(405, 314);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox2);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.bt_connect);
-            this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -166,6 +265,14 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- OMBHostDebug/Form1.cs
+++ OMBHostDebug/Form1.cs
@@ -22,6 +22,14 @@ namespace OMBHostDebug
         public Form1()
         {
             InitializeComponent();
+
+            // Add legend to listbox
+            listBox3.Items.Clear();
+            listBox3.Items.Add("Cmd");
+            listBox3.Items.Add("Red");
+            listBox3.Items.Add("Green");
+            listBox3.Items.Add("Blue");
+            listBox3.Items.Add("Eff");
         }
 
         private void bt_connect_Click(object sender, EventArgs e)
@@ -35,6 +43,7 @@ namespace OMBHostDebug
         private void comboBox1_DropDown(object sender, EventArgs e)
         {
             comboBox1.Items.Clear();
+
             foreach(string port in SerialPort.GetPortNames())
             {
                 comboBox1.Items.Add(port);
@@ -64,7 +73,7 @@ namespace OMBHostDebug
             try
             {
                 listBox2.Items.Clear();
-                listBox2.Items.Add(omb.Buttons[listBox1.SelectedIndex].ButtonNumber);
+                //listBox2.Items.Add(omb.Buttons[listBox1.SelectedIndex].ButtonNumber);
                 listBox2.Items.Add(omb.Buttons[listBox1.SelectedIndex].Command);
                 listBox2.Items.Add(omb.Buttons[listBox1.SelectedIndex].LedColor.R);
                 listBox2.Items.Add(omb.Buttons[listBox1.SelectedIndex].LedColor.G);
@@ -105,6 +114,21 @@ namespace OMBHostDebug
         private void button4_Click(object sender, EventArgs e)
         {
             omb.SetStringCmd(listBox1.SelectedIndex, textBox1.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += OMB.BTDN;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += OMB.BTUP;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += OMB.KEYC;
         }
     }
 }
